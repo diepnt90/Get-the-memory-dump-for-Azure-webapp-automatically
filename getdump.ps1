ce960e122f0ef89b5626736ea0c702a0ab50566f
diepnt <diep90.nt@gmail.com>
2020-03-10 11:39:32 +0700

    Create getdump.ps1

---
+++ getdump.ps1
@@ -0,0 +1,9 @@
+$pair = '$diepazure:k7dBzw0vWf952A5Mu79JuG2DmrsptQ7jslH4pL3fxterdmlxZi0MZWjigLEH'
+$Bytes = [System.Text.Encoding]::UTF8.GetBytes($pair)
+$Encoded = [System.Convert]::ToBase64String($Bytes)
+$basicAuthValue = "Basic $Encoded "
+$headers = @{ Authorization = $basicAuthValue}
+$command="d:\devtools\sysinternals\procdump -accepteula -ma 7316 -m 300 -s 5 -n 1 D:\home\Logfiles\";
+iex $command
+Start-Sleep -s 300
+Invoke-WebRequest -uri "https://diepazure.scm.azurewebsites.net/api/continuouswebjobs/getdump/stop" -Method POST -Headers $headers -ContentType "application/json"
